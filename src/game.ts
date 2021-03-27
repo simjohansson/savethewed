e02db1435122b16db72bda4430cc1f7646d5eabb
Simon Johansson <simjohansson@gmail.com>
2021-03-27 19:00:12 +0100

    Stars!

--- src/game.ts
+++ src/game.ts
@@ -4,6 +4,8 @@ export default class Demo extends Phaser.Scene {
     constructor() {
         super('demo');
     }
+    private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+    private player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
 
     preload() {
         this.load.image('sky', 'assets/sky.png');
@@ -18,10 +20,76 @@ export default class Demo extends Phaser.Scene {
 
     create() {
         this.add.image(400, 300, 'sky');
-        this.add.image(400, 300, 'star');
+        const platforms = this.physics.add.staticGroup();
+
+        platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+        platforms.create(600, 400, 'ground');
+        platforms.create(50, 250, 'ground');
+        platforms.create(750, 220, 'ground');
+
+        this.player = this.physics.add.sprite(100, 450, 'dude');
+
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [{ key: 'dude', frame: 4 }],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.physics.add.collider(this.player, platforms);
+        this.cursors = this.input.keyboard.createCursorKeys();
+        const stars = this.physics.add.group({
+            key: 'star',
+            repeat: 11,
+            setXY: { x: 12, y: 0, stepX: 70 }
+        });
+
+        stars.children.iterate(function (child) {
+
+            (child.body as Phaser.Physics.Arcade.Body).setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+
+        });
+        this.physics.add.collider(stars, platforms);
     }
 
     update() {
+
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-160);
+
+            this.player.anims.play('left', true);
+        }
+        else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(160);
+
+            this.player.anims.play('right', true);
+        }
+        else {
+            this.player.setVelocityX(0);
+
+            this.player.anims.play('turn');
+        }
+
+        if (this.cursors.up.isDown && this.player.body.touching.down) {
+            this.player.setVelocityY(-330);
+        }
     }
 }
 
@@ -29,6 +97,13 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: Demo
 };
 
